f9b709e222698e550c24fb4a39f4a256467ed034
ianpetercreative <ianpetercreative@gmail.com>
2025-01-25 21:32:29 -0700

    learning type assertions

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,10 @@
 "use strict";
 const btn = document.getElementById("btn");
+const input = document.getElementById("todoinput");
+// This is an alternative option:
+// (<HTMLInputElement>input).value
+// However, this second option is not useable in React, so it is less common.
 btn.addEventListener("click", function () {
-    alert("Clicked");
+    alert(input.value);
+    input.value = "";
 });
