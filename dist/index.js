41a32f8c0ff9cba6c017f7cef44c557353fd22cf
ianpetercreative <ianpetercreative@gmail.com>
2025-01-27 06:31:10 -0700

    adding event listenters

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,20 @@
 "use strict";
 const btn = document.getElementById("btn");
 const input = document.getElementById("todoinput");
+const form = document.querySelector("form");
 // This is an alternative option:
 // (<HTMLInputElement>input).value
 // However, this second option is not useable in React, so it is less common.
-btn.addEventListener("click", function () {
-    alert(input.value);
-    input.value = "";
-});
+// btn.addEventListener("click", function() {
+//     alert(input.value);
+//     input.value = "";
+// })
+// form.addEventListener("submit", function(e){
+//     e.preventDefault(); 
+//     console.log("submitted")
+// })
+function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Submitted");
+}
+form.addEventListener("submit", handleSubmit);
